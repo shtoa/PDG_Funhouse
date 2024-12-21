03f90234c0c919527f6b0a07977dc870cf8ce72c
shtoa <howtomatben@gmail.com>
2024-12-21 22:07:59 +0100

    added CorridorGenerationTest Class

---
+++ Assets/Tests/EditMode/CorridorGenerationTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using dungeonGenerator;
+
+public class CorridorGenerationTest
+{
+    [Test]
+    public void CorridorGenerationFullConnection()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    //[Test]
+
+
+}
